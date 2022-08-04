7a347b5b3ba97d68dcb79437ffd5ce5acf2303f6
angel9015 <jc11091005@gmail.com>
2022-08-04 05:15:16 -0400

    Updated the UI

--- app/components/car-component/car-component.js
+++ app/components/car-component/car-component.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
-import { View, Text, ActivityIndicator, Image, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, Image, FlatList, Button, TouchableHighlight } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import Modal from "react-native-modal";
+import Collapsible from 'react-native-collapsible';
 import FilterComponent from '../filter-component/filter-component';
+import Svg, { Path } from "react-native-svg";
 import styles from './styles';
 
 const getErrorMessage = () => (
@@ -10,96 +13,176 @@ const getErrorMessage = () => (
   </Text>
 );
 
-const getCarInfo = (carInfo, searchQuery, makeFilters, yearFilters, colorFilters, yearSliderValue) => {
+const CarComponent = (props) => {
+    const {
+        isLoading,
+        error,
+        fetchData,
+        carInfo,
+    } = props;
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const [makeFilters, setMakeFilters] = React.useState([]);
+    const [yearFilters, setYearFilters] = React.useState([]);
+    const [colorFilters, setColorFilters] = React.useState([]);
+    const [yearSliderValue, setYSliderValue] = React.useState([]);
+    const [carArray, setCarArray] = React.useState([]);
+    const [isCollapsed, setCollapsed] = useState(true);
 
-    // Search by make, model, year, color
-    let carArray =  carInfo.filter(e1 => e1.car.indexOf(searchQuery) > -1 || e1.car_model.indexOf(searchQuery) > -1 || e1.car_model_year.toString().indexOf(searchQuery) > -1) || e1.car_color.indexOf(searchQuery) > -1;
+    const onChangeSearch = query => setSearchQuery(query);
 
-    // filter by make, year, color
-    if(makeFilters.length > 0) {
-        carArray = carArray.filter(e1 => makeFilters.indexOf(e1.car) > -1)
-    }
+    const hasCarData = Object.keys(carInfo).length;
 
-    if(yearFilters.length > 0) {
-        carArray = carArray.filter(e1 => yearFilters.indexOf(e1.car_model_year) > -1)
-    }
+    useEffect(() => {
+        fetchData();
+    }, [])
+   
+    useEffect(() => {
+        if(!hasCarData) {
+            return;
+        }
 
-    if(colorFilters.length > 0) {
-        carArray = carArray.filter(e1 => yearFilters.indexOf(e1.car_color) > -1)
-    }
-    
-    // filter by year slider
-    if(yearSliderValue.length === 2) {
-        carArray = carArray.filter(e1 => e1.car_model_year >= yearSliderValue[0] && e1.car_model_year <= yearSliderValue[1])
-    }
+        // Search by make, model, year, color
+        let carArray =  carInfo.filter(e1 => e1.car.indexOf(searchQuery) > -1 || e1.car_model.indexOf(searchQuery) > -1 || e1.car_model_year.toString().indexOf(searchQuery) > -1) || e1.car_color.indexOf(searchQuery) > -1;
+
+        // filter by make, year, color
+        if(makeFilters.length > 0) {
+            carArray = carArray.filter(e1 => makeFilters.indexOf(e1.car) > -1)
+        }
+
+        if(yearFilters.length > 0) {
+            carArray = carArray.filter(e1 => yearFilters.indexOf(e1.car_model_year) > -1)
+        }
+
+        if(colorFilters.length > 0) {
+            carArray = carArray.filter(e1 => yearFilters.indexOf(e1.car_color) > -1)
+        }
+        
+        // filter by year slider
+        if(yearSliderValue.length === 2) {
+            carArray = carArray.filter(e1 => e1.car_model_year >= yearSliderValue[0] && e1.car_model_year <= yearSliderValue[1])
+        }
+        setCarArray(carArray);
+        
+    }, [hasCarData, searchQuery, makeFilters, yearFilters, colorFilters, yearSliderValue])
+
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const toggleModal = (item) => {
+        setSelectedItem(item);
+    };
 
     return (
-        <FlatList
-            data={carArray}
-            renderItem={({item}) => (
-                <View style={styles.carItem} key={item.id}>
-                    <View>
-                        <Image
-                            style={styles.carImage}
-                            source={{
-                                uri: `https://picsum.photos/200/300?random=${item.id}`, 
-                            }}
-                        />
-                    </View>
-                    <View style={styles.carDetails}>
-                        <View style={styles.carInfo1}>
-                            <Text>Make: {item.car}</Text>
-                            <Text>Model: {item.car_model}</Text>
-                            <Text>Color: {item.car_color}</Text>
-                            <Text>Year: {item.car_model_year}</Text>
+        <View style={styles.container}>
+            {error ? getErrorMessage() : null}
+
+            { selectedItem ? (
+                <Modal 
+                    isVisible={selectedItem ? true: false}
+                    onBackdropPress={() => toggleModal(null)}
+                    animationIn="slideInLeft"
+                    animationOut="slideOutRight"
+                >
+                    <View style={styles.modalView}>
+                        <View style={{flexDirection: 'row'}}>
+                            <Image
+                                style={{width: 120, height: 120, borderRadius: 20}}
+                                source={{
+                                    uri: `https://picsum.photos/200/300?random=${selectedItem.id}`, 
+                                }}
+                            />
+                            <View style={{marginLeft: 20}}>
+                                <Text style={styles.carDetailTxt}>Make: {selectedItem.car}</Text>
+                                <Text style={styles.carDetailTxt}>Model: {selectedItem.car_model}</Text>
+                                <Text style={styles.carDetailTxt}>Year: {selectedItem.car_model_year}</Text>
+                                <Text style={styles.carDetailTxt}>Color: {selectedItem.car_color}</Text>
+                            </View>
                         </View>
-                        <View>
-                            <Text>{item.price}</Text>
+                        <Text style={{marginTop: 5}}>Note: Booking feature is upcoming.</Text>
+                        <View style={styles.btnGroup}>
+                            
+                            <View style={{marginRight: 10}}>
+                                <Button title="Cancel" onPress={() => toggleModal(null)} />
+                            </View>
+                            
+                            <Button title="Book" onPress={() => toggleModal(null)} />
                         </View>
                     </View>
+                </Modal>
+            ) : null}
+            
+            <View style={styles.loading}>
+                {isLoading ? <ActivityIndicator /> : null}
+            </View>
+            {hasCarData ? (
+                <View style={{marginTop: 30}}>
+                    <Text style={styles.title1}>Choose from {hasCarData}</Text>
+                    <Text style={styles.title2}>makes and models</Text>
                 </View>
-            )}
-        />
-    )
-}
-
-const CarComponent = (props) => {
-  const {
-    isLoading,
-    error,
-    fetchData,
-    carInfo,
-  } = props;
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const [makeFilters, setMakeFilters] = React.useState([]);
-  const [yearFilters, setYearFilters] = React.useState([]);
-  const [colorFilters, setColorFilters] = React.useState([]);
-  const [yearSliderValue, setYSliderValue] = React.useState([]);
-
-  const onChangeSearch = query => setSearchQuery(query);
-
-  const hasCarData = Object.keys(carInfo).length;
-
-  useEffect(() => {
-    fetchData();
-  }, [])
-   
-  return (
-    <View style={styles.container}>
-        {error ? getErrorMessage() : null}
-        <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-            style={styles.searchBar}
-        />
-        <View style={styles.loading}>
-            {isLoading ? <ActivityIndicator /> : null}
+            ) : null}
+            {hasCarData ? (
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10}}>
+                    <Text style={{fontSize: 20, fontWeight: 'bold'}}>Search</Text>
+                    <TouchableHighlight 
+                        underlayColor='#ffffff'
+                        style={{flexDirection: 'column', justifyContent: 'center'}}
+                        onPress={() => setCollapsed(!isCollapsed)}>
+                        <View style={{width: 16, height: 16}}>
+                            { isCollapsed ? (
+                                <Svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="black" >
+                                    <Path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                                </Svg>
+                            ) : (
+                                <Svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="black">
+                                    <Path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+                                </Svg>
+                            )}
+                        </View>
+                    </TouchableHighlight>
+                </View>
+            ) : null}
+            <Collapsible collapsed={isCollapsed}>
+                <Searchbar
+                    placeholder="Search"
+                    onChangeText={onChangeSearch}
+                    value={searchQuery}
+                    style={styles.searchBar}
+                />
+            </Collapsible>
+            {hasCarData ? <FilterComponent carInfo={carInfo} setMakeFilters={setMakeFilters} setYearFilters={setYearFilters} setColorFilters={setColorFilters} setYSliderValue={setYSliderValue} /> : null}
+            {hasCarData ? (
+                <FlatList
+                    data={carArray}
+                    style={styles.carList}
+                    numColumns={2}
+                    keyExtractor={(item, index) => index}
+                    renderItem={({item}) => (
+                        <TouchableHighlight style={styles.carItem} key={item.id} underlayColor='#ffffff' onPress={() => toggleModal(item)}>
+                            <View>
+                                <Image
+                                    style={styles.carImage}
+                                    source={{
+                                        uri: `https://picsum.photos/200/300?random=${item.id}`, 
+                                    }}
+                                />
+                                <View style={styles.carDetails}>
+                                    <Text style={styles.carDetailTxt}>{item.car}</Text>
+                                    <Text style={styles.carDetailTxt}>{item.price}</Text>
+                                    <View style={styles.status}>
+                                        {item.availability === true ? (
+                                            <View style={styles.availableStatus}></View>
+                                        ) : (
+                                            <View style={styles.notAvailableStatus}></View>
+                                        )}
+                                        <Text>{`${item.availability === true ? 'Available' : 'Not Available'}`}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </TouchableHighlight>
+                    )}
+                />
+            ) : null}
         </View>
-        {hasCarData ? <FilterComponent carInfo={carInfo} setMakeFilters={setMakeFilters} setYearFilters={setYearFilters} setColorFilters={setColorFilters} setYSliderValue={setYSliderValue} /> : null}
-        {hasCarData ? getCarInfo(carInfo, searchQuery, makeFilters, yearFilters, colorFilters, yearSliderValue): null}
-    </View>
-  );
+    );
 };
 
 export default CarComponent;
--- app/components/car-component/styles.js
+++ app/components/car-component/styles.js
@@ -18,12 +18,12 @@ export default StyleSheet.create({
     marginBottom: 20
   },
   carItem: {
-    flexDirection: 'row',
+    flex: 1,
+    flexDirection: 'column',
+    margin: 5,
     borderRadius: 5,
     borderColor: '#d8d8d8',
     borderWidth: 1,
-    marginBottom: 30,
-    padding: 20,
     shadowOpacity: 0.8,
     shadowRadius: 5,
     shadowOffset: {
@@ -36,18 +36,80 @@ export default StyleSheet.create({
     backgroundColor: 'white'
   },
   carImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 5
+    height: 120,
+    flex: 1,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5
   },
   carDetails: {
     flex: 1,
-    marginLeft: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  carDetailTxt: {
+    fontWeight: 'bold'
+  },
+  status: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    alignItems: 'center',
+    marginTop: 5
+  },
+  availableStatus: {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    backgroundColor: 'green',
+    marginRight: 5
+  },
+  notAvailableStatus: {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    marginRight: 5
   },
   carInfo1: {
     flexDirection: 'column',
     justifyContent: 'space-between'
+  },
+  title1: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24
+  },
+  title2: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  carList: {
+    padding: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#d8d8d8',
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    shadowOffset: {
+        width: 2,
+        height: 2
+    },
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    elevation: 3,
+    backgroundColor: 'white'
+  },
+  btnGroup: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 20
+  },
+  modalView: {
+    backgroundColor: 'white', 
+    padding: 20, 
+    borderRadius: 20
   }
 });
